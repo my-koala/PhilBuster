738000aa56f6229e1aa65c8bcc08a3bad300359a
valedict0 <jamesbrusewitz@gmail.com>
2025-04-17 22:30:00 -0700

    Changed submit button to Speak

--- scripts/session/session_submit.gd
+++ scripts/session/session_submit.gd
@@ -13,17 +13,6 @@ var enabled: bool = true:
 			enabled = value
 			_dirty = true
 
-@export
-var sentence_complete: bool = false:
-	get:
-		return sentence_complete
-	set(value):
-		if sentence_complete != value:
-			sentence_complete = value
-			_dirty = true
-
-@onready
-var _rich_text_label: RichTextLabel = %rich_text_label as RichTextLabel
 @onready
 var _button: Button = %button as Button
 
@@ -41,12 +30,3 @@ func _physics_process(delta: float) -> void:
 	_dirty = false
 	
 	_button.disabled = !enabled
-	
-	_rich_text_label.text = ""
-	if enabled:
-		if sentence_complete:
-			_rich_text_label.append_text("[color=green][b]READY![/b][/color]\n")
-			_rich_text_label.append_text("[color=white][i]Sentence is Complete![/i][/color]")
-		else:
-			_rich_text_label.append_text("[color=red][b]WARNING![/b][/color]\n")
-			_rich_text_label.append_text("[color=white][i]Sentence is Incomplete![/i][/color]")
