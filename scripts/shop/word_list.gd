94da6519721159326bb3155ca3fba4287b5a83cb
valedict0 <jamesbrusewitz@gmail.com>
2025-04-20 14:46:22 -0700

    Fixed buttons

--- scripts/shop/word_list.gd
+++ scripts/shop/word_list.gd
@@ -1,6 +1,8 @@
 extends Panel
 class_name WordList
 
+const GAME_BUTTON: PackedScene = preload("res://assets/game_button/game_button.tscn")
+
 signal list_changed()
 signal on_word_hovered(card: CardInfo, hovered: bool)
 
@@ -10,13 +12,8 @@ var label: Label
 @export
 var container: Container
 
-@onready
-var _sfx_manager: ButtonSFXManager = $button_sfx_manager as ButtonSFXManager
-
 var _game_stats: GameStats
 var _cards: Array[CardInfo]
-
-const BUTTON: PackedScene = preload("res://assets/shop/word_list_button.tscn")
 
 func init(name: String, cards: Array[CardInfo], game_stats: GameStats) -> void:
 	label.text = name
@@ -29,9 +26,8 @@ func init(name: String, cards: Array[CardInfo], game_stats: GameStats) -> void:
 	#for card: CardInfo in cards:
 	for i: int in range(cards.size()):
 		var card: CardInfo = cards[i]
-		var button: Button = BUTTON.instantiate() as Button
-		# ABANDON ALL HOPE ALL YE WHO ENTER HERE
-		#_sfx_manager.hook_to_button(button)
+		var button: GameButton = GAME_BUTTON.instantiate() as GameButton
+		button.toggle_mode = true
 		button.text = card.get_word()
 		button.set_pressed_no_signal(game_stats.deck_has(card))
 		button.toggled.connect(func(toggled: bool) -> void: _on_card_button_toggled(button, card, toggled))
